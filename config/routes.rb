6ce0011d2a10402c7fb4ed0f6bc9655ecf14f63a
Rodrigo Von Bischoffshausen <rodrigo.von.ing@gmail.com>
2023-06-03 15:36:31 -0400

    Se crean rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get '/home', to: 'pages#home'
+  post '/offers', to: 'offers#index'
+  post '/products', to: 'products#create'
+  put '/products/:id', to: 'products#update'
+  delete '/products/:id', to: 'products#destroy'
+  post '/bookings', to: 'bookings#create'
+  post '/reviews', to: 'reviews#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
