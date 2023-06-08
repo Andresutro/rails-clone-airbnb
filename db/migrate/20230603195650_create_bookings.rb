ec6e94ff4940fd882d7a75edb2c6baa280b599b8
Rodrigo Von Bischoffshausen <rodrigo.von.ing@gmail.com>
2023-06-08 19:22:16 -0400

    Se avanza en cambios de la versión 2 del CRUD de Bookings

--- db/migrate/20230603195650_create_bookings.rb
+++ db/migrate/20230603195650_create_bookings.rb
@@ -1,6 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[7.0]
   def change
     create_table :bookings do |t|
+      t.references :user, foreign_key: true
+      t.references :housting, foreign_key: true
       t.integer :user_id
       t.integer :housting_id
       t.date :start_date
